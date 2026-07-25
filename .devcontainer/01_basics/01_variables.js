8a0db610605fcdaa8098318d10da7ac86c0acc0a
RISHAV KAMAL <56732972+RishavKamal@users.noreply.github.com>
2026-07-25 11:34:54 +0000

    day1

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,19 @@ const accountId = 144553
 let accountEmail = "rishavkamalbg821@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-accountId = 2
+//accountId = 2 //not allowed
+
+accountEmail= "hc@hc.com"
+accountPassword= "21212121"
+accountCity= "Bengaluru"
+
+/*
+prefer not to use var because of 
+issue in block scope and functional scope
+*/
 
 console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Rishav")
+
+let name= "Rishav"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned 
+// symbol =>unique
+
+// object 
+
+console.log(typeof "Rishav");
+
