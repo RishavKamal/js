2c5d23daad5aa3d433dbb26b68c9368dd5d2ed4e
RISHAV KAMAL <56732972+RishavKamal@users.noreply.github.com>
2026-08-08 16:14:08 +0000

    maths

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,28 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//----------------Maths------------------//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(2.1));
+// console.log(Math.round(7.3));
+// console.log(Math.floor(1.8));
+
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 0
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
